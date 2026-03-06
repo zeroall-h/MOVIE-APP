527143b25a271966e4444ea55e639ab1e4c9a27a
허다영 <zeroall@hivelab.co.kr>
2026-03-06 14:12:01 +0900

    [Update] Movie.js > 포스터 사이즈 상수화

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -1,10 +1,12 @@
 import { Component } from '../core/heropy'
 import movieStore, { getMovieDetails } from '../store/movie'
 
+const POSTER_SIZE_SMALL = 'SX300'
+const POSTER_SIZE_LARGE = 'SX700'
+
 export default class Movie extends Component {
   async render() {
     this.el.classList.add('container', 'the-movie')
-    // 스켈레톤 UI 출력!
     this.el.innerHTML = /* html */ `
       <div class="poster skeleton"></div>
       <div class="specs">
@@ -13,21 +15,23 @@ export default class Movie extends Component {
         <div class="plot skeleton"></div>
       </div>
     `
-    // 영화 상세 정보 가져오기!
     await getMovieDetails(history.state.id)
     const { movie } = movieStore.state
-    const bigPoster = movie.Poster.replace('SX300', 'SX700')
+    const bigPoster = movie.Poster.replace(POSTER_SIZE_SMALL, POSTER_SIZE_LARGE)
 
-    // this.el.classList.add('container', 'the-movie')
     this.el.innerHTML = /* html */ `
       <div
         style="background-image: url(${bigPoster});"
         class="poster">
       </div>
+      ${this._renderSpecs(movie)}
+    `
+  }
+
+  _renderSpecs(movie) {
+    return /* html */ `
       <div class="specs">
-        <div class="title">
-          ${movie.Title}
-        </div>
+        <div class="title">${movie.Title}</div>
         <div class="labels">
           <span>${movie.Released}</span>
           &nbsp;/&nbsp;
@@ -35,32 +39,26 @@ export default class Movie extends Component {
           &nbsp;/&nbsp;
           <span>${movie.Country}</span>
         </div>
-        <div class="plot">
-          ${movie.Plot}
-        </div>
-        <div>
-          <h3>Ratings</h3>
-          ${movie.Ratings.map(rating => {
-            return `<p>${rating.Source} - ${rating.Value}</p>`
-          }).join('')}
-        </div>
-        <div>
-          <h3>Actors</h3>
-          <p>${movie.Actors}</p>
-        </div>
-        <div>
-          <h3>Director</h3>
-          <p>${movie.Director}</p>
-        </div>
-        <div>
-          <h3>Production</h3>
-          <p>${movie.Production}</p>
-        </div>
-        <div>
-          <h3>Genre</h3>
-          <p>${movie.Genre}</p>
-        </div>
+        <div class="plot">${movie.Plot}</div>
+        ${this._renderSection('Ratings', this._renderRatings(movie.Ratings))}
+        ${this._renderSection('Actors', `<p>${movie.Actors}</p>`)}
+        ${this._renderSection('Director', `<p>${movie.Director}</p>`)}
+        ${this._renderSection('Production', `<p>${movie.Production}</p>`)}
+        ${this._renderSection('Genre', `<p>${movie.Genre}</p>`)}
       </div>
     `
+  }
+
+  _renderSection(title, content) {
+    return /* html */ `
+      <div>
+        <h3>${title}</h3>
+        ${content}
+      </div>
+    `
+  }
+
+  _renderRatings(ratings) {
+    return ratings.map(({ Source, Value }) => `<p>${Source} - ${Value}</p>`).join('')
   }
 }
